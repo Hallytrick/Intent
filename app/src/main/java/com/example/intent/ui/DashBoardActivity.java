57f9c56c31e060bd9894416eba18221d5eb5ebc1
Caleb-Komen <calebkipyatich@gmail.com>
2020-10-30 00:21:29 +0300

    Implement about us page

--- app/src/main/java/com/example/intent/ui/DashBoardActivity.java
+++ app/src/main/java/com/example/intent/ui/DashBoardActivity.java
@@ -4,22 +4,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.intent.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class DashBoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class DashBoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        View.OnClickListener {
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
+    private CardView aboutUsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,9 @@ public class DashBoardActivity extends AppCompatActivity implements NavigationVi
 
         initToolbar();
         initNavDrawer();
+        aboutUsCard = findViewById(R.id.about_us_card);
+        aboutUsCard.setOnClickListener(this);
+
     }
 
     private void initNavDrawer() {
@@ -73,5 +85,37 @@ public class DashBoardActivity extends AppCompatActivity implements NavigationVi
             default:
                 return true;
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.about_us_card:
+                showAboutDialog();
+                break;
+        }
+    }
+
+    private void showAboutDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_about_us);
+        dialog.setCancelable(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        params.copyFrom(dialog.getWindow().getAttributes());
+        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        dialog.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(params);
     }
 }
